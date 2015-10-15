14d51ea3bb450ea6b02ac6992515ef11ae215e7e
Charles Xue <charlesx2013@gmail.com>
2015-10-14 22:58:50 -0700

    moved constants into another file and added imports

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,6 +1,3 @@
-// This file is not implemented yet and isn't used anywhere.
-// TODO(sam): Implement this file
-
 const ROOM_REGEX = /room=(\w+)/;
 const USER_ID_REGEX = /user_id=(\w+)/;
 
@@ -11,27 +8,25 @@ const getAttributeFromUrlParams = (regex, attr) => {
 };
 
 // Grab room from URL
-const URL_REGEX = /room=(\w+)/;
-const ROOM_PARAMS = URL_REGEX.exec(location.search);
+const ROOM_PARAMS = ROOM_REGEX.exec(location.search);
 if (!ROOM_PARAMS) { throw new Error('Missing room in URL!'); }
 
 
 // Grab user_id from URL
-const USER_ID_REGEX = /user_id=(\w+)/;
 const USER_ID_PARAMS = USER_ID_REGEX.exec(location.search);
 if (!USER_ID_PARAMS) { throw new Error('Missing user_id in URL!'); }
 
-const BASE_URL = `https://research-chat-room.firebaseio.com/${ROOM}`;
+const BASE_URL = `https://research-chat-room.firebaseio.com/${getAttributeFromUrlParams(ROOM_REGEX)}`;
 
 const CHAT_CONSTANTS = {
-  ROOM: ROOM_PARAMS[1],
-  USER_ID: USER_ID_PARAMS[1],
+  ROOM: getAttributeFromUrlParams(ROOM_REGEX),
+  USER_ID: getAttributeFromUrlParams(USER_ID_REGEX),
   BASE_URL: BASE_URL,
   USERS_FIREBASE: new Firebase(`${BASE_URL}/users`),
 
   // Time a room is open.
   ROOM_OPEN_TIME: 180000, // 3 minutes
-  ONE_MIN_WARNING: ROOM_OPEN_TIME - 60000, // 1 minute warning
+  WARNING: 60000, // 1 minute warning
 
   // Users needed per room
   USERS_PER_ROOM: 3,
@@ -40,4 +35,4 @@ const CHAT_CONSTANTS = {
   MAX_WAITING_TIME: 180000, // 3 minutes
 };
 
-export default CHAT_CONSTANTS;
+export { CHAT_CONSTANTS };
