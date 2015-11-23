5a52d1ffe1f3320dd540418793f5c85c93783c22
Sam Lau <samlau95@gmail.com>
2015-11-23 01:46:04 -0800

    Set current study on creation so defaults are always present

--- src/js/admin/actions/AdminActions.js
+++ src/js/admin/actions/AdminActions.js
@@ -2,7 +2,12 @@ import alt from '../alt';
 
 class AdminActions {
   constructor() {
-    this.generateActions('addStudy', 'setSelectedStudy');
+    this.generateActions('setSelectedStudy');
+  }
+
+  addStudy(study) {
+    this.dispatch(study);
+    this.actions.setSelectedStudy(study);
   }
 }
 
