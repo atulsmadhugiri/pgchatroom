03d85a15a55932191e5b495f5bcc61e6b89610f8
Sam Lau <samlau95@gmail.com>
2016-02-26 02:33:51 -0800

    Use hot loading

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,7 @@ module.exports = {
         loaders: [
             { test: require.resolve("react"), loader: "expose?React" },
             { test: /\.s?css$/, loader: "style!css!sass" },
-            // { test: /\.jsx?$/, loaders: ["react-hot", "babel"], exclude: /node_modules/ }
-            { test: /\.jsx?$/, loaders: ["babel?optional=runtime"], exclude: /node_modules/ }
+            { test: /\.jsx?$/, loaders: ["react-hot", "babel?optional=runtime"], exclude: /node_modules/ }
         ]
     },
     plugins: [
