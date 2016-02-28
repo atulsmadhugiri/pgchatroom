87e646d1be77a6df92541a14d09879b16f0b5830
Sam Lau <samlau95@gmail.com>
2016-02-28 13:25:48 -0800

    Use ROOT_URL from constants

--- src/js/json_to_csv/actions.js
+++ src/js/json_to_csv/actions.js
@@ -1,6 +1,8 @@
 import { createAction } from 'redux-actions';
 import fetch from 'isomorphic-fetch';
 
+import { ROOT_URL } from '../constants';
+
 export const setStudy = createAction('SET_STUDY');
 export const setDisplayOption = createAction('SET_DISPLAY_OPTION');
 export const startDataFetch = createAction('START_DATA_FETCH');
@@ -11,7 +13,7 @@ export function setStudyAndStartFetch(study) {
     dispatch(setStudy(study));
     dispatch(startDataFetch(study));
 
-    return fetch(`https://research-chat-room.firebaseio.com/${study}.json`)
+    return fetch(`${ROOT_URL}/${study}.json`)
       .then(response => response.json())
       .then(json => dispatch(setData(json)));
   };
