d25dd2305f29dae6de988522798daa655846b216
Sam Lau <samlau95@gmail.com>
2016-02-28 13:26:00 -0800

    Extract data functionality

--- src/js/json_to_csv/components.js
+++ src/js/json_to_csv/components.js
@@ -3,40 +3,52 @@ import React from 'react';
 import { connect } from 'react-redux';
 import BabyParse from 'babyparse';
 
-import { setStudyAndStartFetch } from './actions';
+import { setStudyAndStartFetch, setDisplayOption } from './actions';
 
-const USER_CSV_FIELDS = ['Room', 'Room ID', 'User ID'];
 
-// Extracts Room,Room ID,User ID from JSON
-const parseUserData = (json) => {
-  const data = [];
+// Extracts Room,Room ID,User ID from data
+function extractUserData(data) {
+  const USER_CSV_FIELDS = ['Room', 'Room ID', 'User ID'];
+  const csvData = [];
 
-  _.mapObject(json, (roomTypeData, roomType) => {
+  _.mapObject(data, (roomTypeData, roomType) => {
     _.mapObject(roomTypeData.rooms, (roomData, room) => {
       _.mapObject(roomData.users, (userVal, user) => {
-        data.push([roomType, room, user]);
+        csvData.push([roomType, room, user]);
       });
     });
   });
 
-  return data;
-};
+  return BabyParse.unparse({ fields: USER_CSV_FIELDS, data: csvData });
+}
 
-const userDataToCsv = (data) => {
-  return BabyParse.unparse({ fields: USER_CSV_FIELDS, data: data });
-};
+// Extracts Room ID,User ID,Message from data
+function extractMessageData(data) {
+  const MESSAGES_CSV_FIELDS = ['Room ID', 'User ID', 'Message'];
+  const csvData = [];
+
+  _.mapObject(data, (roomTypeData, roomType) => {
+    _.mapObject(roomTypeData.rooms, (roomData, room) => {
+      _.each(roomData.messages, ({ message, userId }) => {
+        csvData.push([room, userId, message]);
+      });
+    });
+  });
+
+  return BabyParse.unparse({ fields: MESSAGES_CSV_FIELDS, data: csvData });
+}
 
 const DataDisplay = ({ displayOption, data }) => {
   let csv;
   switch (displayOption) {
   case 'USERS':
-    csv = userDataToCsv(parseUserData(data));
+    csv = extractUserData(data);
     break;
   case 'MESSAGES':
-    csv = 'messages';
+    csv = extractMessageData(data);
     break;
   default:
-    csv = 'No data';
+    csv = 'No data available for this study.';
   }
 
   return <pre>{data && csv}</pre>;
@@ -60,6 +72,14 @@ const JSONToCSV = ({ study, displayOption, data, dispatch }) => {
     </form>
 
     <br />
+    {data && <form>
+      <input type="radio" value="USERS"
+        checked={displayOption === 'USERS'}
+        onChange={(e) => dispatch(setDisplayOption(e.target.value))} /> Users
+      <input type="radio" value="MESSAGES"
+        checked={displayOption === 'MESSAGES'}
+        onChange={(e) => dispatch(setDisplayOption(e.target.value))} /> Messages
+    </form>}
     <DataDisplay displayOption={displayOption} data={data} />
   </div>);
 };
