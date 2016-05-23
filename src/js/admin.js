6b1fc6ddf0d8f67367ef363a2c2fd9de309e1944
Charles Xue <charlesx2013@gmail.com>
2016-05-22 22:27:02 -0700

    Skeleton of roomList

--- src/js/admin.js
+++ src/js/admin.js
@@ -11,6 +11,7 @@ import ConfigSetter from './admin/components/ConfigSetter';
 import StudyList from './admin/components/StudyList';
 import Spacer from './admin/components/Spacer';
 import RoomGenerator from './admin/components/RoomGenerator';
+import RoomsList from './admin/components/RoomsList';
 
 import AdminActions from './admin/actions/AdminActions';
 import AdminStore from './admin/stores/AdminStore';
@@ -23,6 +24,7 @@ function getStateFromStores() {
     selectedStudy: AdminStore.get('selectedStudy'),
     studies: AdminStore.get('studies'),
     constantsFb: AdminStore.get('constantsFb'),
+    roomsFb: AdminStore.get('roomsFb');
   };
 }
 
@@ -75,6 +77,7 @@ const AdminApp = React.createClass({
         {!this.state.selectedStudy ? 'No Study Selected' :
           <div>
             <hr />
+            <RoomsList firebase={this.state.roomsFb} />
             <RoomGenerator selectedStudy={this.state.selectedStudy} />
             <ConfigSetter firebase={this.state.constantsFb}
               study={this.state.selectedStudy} />
--- src/js/admin/components/RoomsList.jsx
+++ src/js/admin/components/RoomsList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import _ from 'underscore';
 
+const DEFAULT_ROOM_LIST = {};
+
 const RoomsList = React.createClass({
 
   getInitialState() {
@@ -10,44 +12,70 @@ const RoomsList = React.createClass({
     }
   },
 
+  componentWillMount() {
+    this._loadRooms(this.props);
+  },
 
+  componentWillRecieveProps(nextProps) {
+    this.props.firebase.off();
+    this.loadRooms(nextProps);
+  },
 
-  _renderTable() {
+  _loadRooms(props) {
+    props.firebase.on('value', snapshot => {
+      if (!snapshot.val()) {
+        props.firebase.set(DEFAULT_ROOM_LIST, (err) => {
+          console.log(err);
+        });
+      } else {
+        this.setState({
+          loaded: true,
+          rooms: snapshot.val(),
+        });
+      }
+    });
+  },
+
+  _removeRoom() {
+    // Removes room
+  }
+
+  _renderRoomsTable() {
+    if (_.isEmpty(this.state.rooms)) {
+      return '';
+    }
+
+    const tableStyle = {
+      margin: '0 auto',
+    };
+
     return (
+      <h3>List of Rooms</h3>
       <table style={tableStyle}>
         <tbody>
           <tr>
-            <th>Time (minutes)</th>
-            <th>Message</th>
-            <th>Type</th>
+            <th>Room</th>
             <th>Delete</th>
           </tr>
-          {this._renderRooms()}
         </tbody>
       </table>
     );
   },
 
   _renderRooms() {
-
-  }
+    return _.keys(this.state.rooms).map(key => {
+      return (
+        <tr key={key}>
+          <td>{key}</td>
+          <td onClick={this._removeRoom}>&times;</td>
+        </tr>
+      );
+    });
+  },
 
   render() {
-    return (
-      <table style={tableStyle}>
-        <tbody>
-          <tr>
-            <th>Time (minutes)</th>
-            <th>Message</th>
-            <th>Type</th>
-            <th>Delete</th>
-          </tr>
-          {this._renderMessages()}
-        </tbody>
-      </table>
-    );
-  }
-
+    return <div>{this._renderRoomsTable()}</div>;
+  },
 });
 
 export default RoomsList;
