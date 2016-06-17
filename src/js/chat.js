eba42043a0f4aa320a557e8ffc9d43c6480d5769
Sam Lau <samlau95@gmail.com>
2016-06-16 23:14:43 -0700

    Don't swallow promise errors

--- src/js/chat.js
+++ src/js/chat.js
@@ -6,6 +6,11 @@ import ReactDOM from 'react-dom';
 
 import ChatApp from './chat/components/ChatApp';
 
+// Don't swallow errors in promises
+process.on('unhandledRejection', (error, promise) => {
+  console.error('[UNHANDLED REJECTION] ', error.stack);
+});
+
 $(() => {
   ReactDOM.render(
     <ChatApp />,
