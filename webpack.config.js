ed37d09b7cd3c1267739279429300059365c95ef
Atul Madhugiri <atulm@berkeley.edu>
2018-12-26 18:59:07 -0800

    added webpack-cli and eslint dependencies; fixed up webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         admin: ["./src/js/admin.js"],
     },
     resolve: {
-        extensions: ["", ".webpack.js", ".web.js", ".js", ".jsx"]
+        extensions: ["*", ".webpack.js", ".web.js", ".js", ".jsx"]
     },
     output: {
         path: __dirname + "/build",
@@ -18,20 +18,43 @@ module.exports = {
         ignored: /node_modules/,
     },
     module: {
-        preLoaders: [
-            { test: /\.jsx?$/, loader: "eslint", exclude: /node_modules/ }
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.jsx?$/,
+                loader: 'eslint',
+                exclude: /node_modules/,
+            },
+            {
+                test: require.resolve("react"),
+                loader: "expose?React"
+            },
+            {
+                test: require.resolve("firebase"),
+                loader: "expose?Firebase"
+            },
+            {
+                test: /\.s?css$/, 
+                loader: "style!css!sass"
+            },
+            {
+                test: /\.jsx?$/,
+                loaders: ["react-hot", "babel?optional=runtime"],
+                exclude: /node_modules/
+            }
         ],
-        loaders: [
-            { test: require.resolve("react"), loader: "expose?React" },
-            { test: require.resolve("firebase"), loader: "expose?Firebase" },
-            { test: /\.s?css$/, loader: "style!css!sass" },
-            { test: /\.jsx?$/, loaders: ["react-hot", "babel?optional=runtime"], exclude: /node_modules/ }
-        ]
+
+        // loaders: [
+        //     { test: require.resolve("react"), loader: "expose?React" },
+        //     { test: require.resolve("firebase"), loader: "expose?Firebase" },
+        //     { test: /\.s?css$/, loader: "style!css!sass" },
+        //     { test: /\.jsx?$/, loaders: ["react-hot", "babel?optional=runtime"], exclude: /node_modules/ }
+        // ]
     },
     plugins: [
-      new webpack.NoErrorsPlugin(),
+      new webpack.NoEmitOnErrorsPlugin(),
     ],
-    eslint: {
-        configFile: ".eslintrc"
-    }
+    // eslint: {
+    //     configFile: ".eslintrc"
+    // }
 };
