c7a5c8cd30d051babe9cdf5f3bc094159dd98b2e
Atul Madhugiri <atulm@berkeley.edu>
2018-12-27 16:34:27 -0800

    Cleaned up LoginButton

--- src/js/admin/components/LoginButton.jsx
+++ src/js/admin/components/LoginButton.jsx
@@ -15,7 +15,7 @@ class LoginButton extends React.Component {
   loginPopup() {
     this.props.fb.authWithOAuthPopup('google', (err, auth) => {
       if (err) {
-        AdminActions.setAuthError(`${err.toString()} | Send Sam your UID:
+        AdminActions.setAuthError(`${err.toString()} | Send Atul your UID:
           ${auth.uid} if you believe this is an error.`);
       } else {
         // We're listening to auth so the store will get the new value
