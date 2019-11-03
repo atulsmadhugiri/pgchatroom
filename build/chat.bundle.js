c2fbfe862701d44ce1f96fa28c5c9b5d752c1f69
Atul Madhugiri <atulm@berkeley.edu>
2019-11-03 11:34:29 -0800

    stupid debug

--- build/chat.bundle.js
+++ build/chat.bundle.js
@@ -91366,7 +91366,7 @@
 	  if (usersPerRoom === 1) {
 		return 'You have been matched to 1 other participant. ' + ('You have ' + (0, _util.convertToMins)(roomOpenTime) + ' minutes to chat.');
 	  }
-	  return 'You have been matched to ' + (usersPerRoom - 1) + ' other participants. ' + ('You have ' + (0, _util.convertToMins)(roomOpenTime) + ' minutes to chat.');
+	  return 'ATUL1You have been matched to ' + (usersPerRoom - 1) + ' other participants. ' + ('You have ' + (0, _util.convertToMins)(roomOpenTime) + ' minutes to chat.');
 	}
 
 	function _finishMessage(password) {
