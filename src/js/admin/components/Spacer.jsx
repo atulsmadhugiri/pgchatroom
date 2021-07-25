448640e08212373127a95fd20c6a6074d3444600
Atul Madhugiri <atulm@berkeley.edu>
2021-07-25 19:10:14 -0400

    [admin] Turn `Spacer` into functional component

--- src/js/admin/components/Spacer.jsx
+++ src/js/admin/components/Spacer.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 /**
  * Just used to put some vertical space in between components; no functionality.
  */
-const Spacer = React.createClass({
-  propTypes: {
-    height: PropTypes.number,
-  },
-
-  render() {
-    const styles = {
-      width: '100%',
-      height: this.props.height || 30,
-    };
-
-    return <div style={styles} />;
-  },
-});
+function Spacer(props) {
+  const { height } = props;
+  const styles = {
+    width: '100%',
+    height: height || 30,
+  };
+  return <div style={styles} />;
+}
 
 export default Spacer;
