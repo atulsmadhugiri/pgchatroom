d139120a157cb49fdb02ad85e9cd29b9406ef5e2
Atul Madhugiri <atulm@berkeley.edu>
2021-07-25 19:11:29 -0400

    Modify `constants` for "new" firebase auth method

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,8 +1,11 @@
 const ROOT_URL = 'https://research-chat-room.firebaseio.com';
-// const ROOT_URL = `ws://127.0.1:5000`;
+const AUTH_DOMAIN = 'research-chat-room.firebaseapp.com';
+
 const STUDIES_URL = `${ROOT_URL}/studies`;
 const CONSTANTS_URL = `${ROOT_URL}/constants`;
 const USER_AUTH_URL = `${ROOT_URL}/user_auth`;
+// this is fine
+const API_KEY = 'AIzaSyABoxVIbZ20Aj7frVFLPu4erpxtONVgRE8';
 
 const BASE_CHAT_ROOM_URL = 'https://www.samlau.me/pg-chat-room';
 
@@ -22,6 +25,8 @@ const MESSAGE_TYPES = {
 
 export {
   ROOT_URL,
+  AUTH_DOMAIN,
+  API_KEY,
   STUDIES_URL,
   CONSTANTS_URL,
   USER_AUTH_URL,
